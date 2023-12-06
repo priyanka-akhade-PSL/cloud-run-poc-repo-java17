1397a23b083f28a66847555b71d73a0943351cf7
priyanka-akhade-PSL <152272736+priyanka-akhade-PSL@users.noreply.github.com>
2023-12-06 17:41:11 +0530

    Initial commit

---
+++ src/test/java/com/logmonitoring/CloudRunPocAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudRunPocAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
